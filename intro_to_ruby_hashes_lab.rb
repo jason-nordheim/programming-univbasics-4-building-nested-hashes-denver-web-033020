a4951e6610278a17cfa0f48459830eaedee5c980
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-05 19:20:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-	{
-		:tier_1 => {
-			:teir_2 => "2"
-		}
-	}
+
 end
 
 def monopoly_with_third_tier
